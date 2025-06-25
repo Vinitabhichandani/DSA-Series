b869de3b52f9639ebede2d138740fa87fcaaf102
Vinit Abhichandani <154498055+Vinitabhichandani@users.noreply.github.com>
2025-06-25 11:17:55 +0530

    Update copyRandomList.java

--- copyRandomList.java
+++ copyRandomList.java
@@ -5,8 +5,6 @@
 
 
 
-// import java.util.*;
-
 
 // class copyRandomList{
 
@@ -22,14 +20,14 @@
 
 //     Node newhead = new Node (head.val);
 //     map.put(head, newhead);
-//     Node oldtemp = head.next; // because when the oldtemp go to the next then copynode is make
+//     Node oldtemp = head.next; // because when the oldtemp go to the next then copynode 
 //     Node newtemp = newhead;
 
 
 
 
 //     while(oldtemp != null){
-//       Node copyNode = new Node(oldtemp.val); // old temp na kar ne copy bati jay che
+//       Node copyNode = new Node(oldtemp.val); // 
 //       map.put(oldtemp, copyNode);
 //       newtemp.next = copyNode; 
 //       oldtemp = oldtemp.next;
@@ -52,154 +50,5 @@
 // }
 
 
-
-
-// import java.util.*;
-
-// class copyRandomList{
-
-//   public Node copyRandomList(Node head){
-
-//     if(head == null){
-//       return null;
-//     }
-
-
-//     HashMap<Node, Node> map = new HashMap<>(); // Node , Node  ma oldtemp ane newtemp store karva mate che
-
-//     Node newhead = new Node(head.val);
-//     map.put(head, newhead);
-
-//     Node oldtemp = head.next; // oldtemp next thase to copyNode banse
-//     Node newtemp = newhead; // newtemp ma newhead store kare che
-
-
-//     while(oldtemp != null){
-
-//       Node copyNode = new Node(oldtemp.val); // oldtemp.next thi copyNode banse
-//       map.put(oldtemp, copyNode);
-//       newtemp.next = copyNode; // jem jem newtemp.next thase tem tem copyNode banse
-
-//       oldtemp = oldtemp.next;
-//       newtemp = newtemp.next;
-//     }
-
-//     oldtemp = head;
-//     newtemp = newhead;
-
-//      // random
-
-//      oldtemp = head;
-//      newtemp = newhead;
-
-//      while(oldtemp != null){
-//       newtemp.random = map.get(oldtemp.random);
-//       oldtemp = oldtemp.next;
-//       newtemp = newtemp.next;
-
-//      }
-//      return newhead;
-//   }
-// }
-
-
-
-
-import java.util.*;
-
-
-class copyRandomList{
-  public Node copyRandomList(Node head){
-
-    if(head == null){ // if the head is null return null thay che
-      return null; 
-  }
-
-  HashMap<Node, Node> map = new HashMap<>(); // Node, Node ma oldtemp ane newtemp store karuyu che
-
-  Node newhead = new Node(head.val);// newhead ma head.val store thay che
- map.put(head, newhead);
- Node oldtemp = head.next; // jem jem head.next thato jay tem tem copyNode start thi banse
- Node newtemp = newhead;
-
-  while(oldtemp != null){
-    Node copyNode = new Node(oldtemp.val); // newhead e oldtemp.val ne copy karto jay ane agan Node banave che
-    map.put(oldtemp, copyNode); // kem store karvayu map ma // oldtemp.next thi copyNode banse
-    newtemp.next = copyNode;
-
-
-    oldtemp = oldtemp.next;
-    newtemp = newtemp.next;
-  }
-
-
-  // Random
-
-  oldtemp = head;
-  newtemp = newhead;
-
-  while(oldtemp != null){
-
-    newtemp.random = map.get(oldtemp.random);
-
-    oldtemp = oldtemp.next;
-    newtemp = newtemp.next;
-  }
-  return newhead;
-   }
-}
-
-
-
-
-
-
-
-import java.util.*;
-
-class Solution{
-
-  public Node copyRandomList(Node head){
-
-    if(head == null){
-      return null;
-    }
-
-
-  HashMap<Node, Node> map = new  HashMap<>(); // basically store in map oldtemp and newtemp
-
-  Node newhead = new Node(head.val); // head ni val thi newhead bane che
-  map.put(head, newhead);
-
-  Node oldtemp = head.next;
-  Node newtemp = newhead;
-
-  while(oldtemp != null){
-
-    Node copyNode = new Node(oldtemp.val);
-    newtemp.next = copyNode;
-
-    map.put(oldtemp, copyNode);
-    oldtemp = oldtemp.next;
-    newtemp = newtemp.next;
-  }
-
-
-  // random pointer
-
-  oldtemp = head;
-  newtemp = newhead;
-
-  while(oldtemp != null){
-
-  newtemp.random = map.get(oldtemp.random);
-
-
-  oldtemp = oldtemp.next;
-  newtemp = newtemp.next;
-   }
-   return newhead; 
- }
-}
 
 
