405adb017d87d9bd3edff1041acc872afa0e694f
Vinit Abhichandani <154498055+Vinitabhichandani@users.noreply.github.com>
2025-06-25 11:31:23 +0530

    Update copyRandomList.java

--- copyRandomList.java
+++ copyRandomList.java
@@ -6,48 +6,48 @@
 
 
 
-// class copyRandomList{
+class copyRandomList{
 
-//   public Node copyRandomList(Node head){
+  public Node copyRandomList(Node head){
 
-//     if(head == null){ // if the head null so return null
-//       return null;
-//     }
-
-
-
-//     HashMap<Node , Node> map = new HashMap<>(); // <Node, Node> in oldtemp, and newtemp
-
-//     Node newhead = new Node (head.val);
-//     map.put(head, newhead);
-//     Node oldtemp = head.next; // because when the oldtemp go to the next then copynode 
-//     Node newtemp = newhead;
+    if(head == null){ // if the head null so return null
+      return null;
+    }
 
 
 
+    HashMap<Node , Node> map = new HashMap<>(); // <Node, Node> in oldtemp, and newtemp
 
-//     while(oldtemp != null){
-//       Node copyNode = new Node(oldtemp.val); // 
-//       map.put(oldtemp, copyNode);
-//       newtemp.next = copyNode; 
-//       oldtemp = oldtemp.next;
-//       newtemp = newtemp.next;
-//     }
+    Node newhead = new Node (head.val);
+    map.put(head, newhead);
+    Node oldtemp = head.next; // because when the oldtemp go to the next then copynode 
+    Node newtemp = newhead;
 
 
-//     oldtemp = head;
-//       newtemp = newhead;
 
-//     // random
-//     while(oldtemp != null){
-//       newtemp.random = map.get(oldtemp.random);
-//       oldtemp = oldtemp.next;
-//       newtemp = newtemp.next;
+
+    while(oldtemp != null){
+      Node copyNode = new Node(oldtemp.val); // 
+      map.put(oldtemp, copyNode);
+      newtemp.next = copyNode; 
+      oldtemp = oldtemp.next;
+      newtemp = newtemp.next;
+    }
+
+
+    oldtemp = head;
+      newtemp = newhead;
+
+    // random
+    while(oldtemp != null){
+      newtemp.random = map.get(oldtemp.random);
+      oldtemp = oldtemp.next;
+      newtemp = newtemp.next;
       
-//     }
-//     return newhead;
-//   }
-// }
+    }
+    return newhead;
+  }
+}
 
 
 
